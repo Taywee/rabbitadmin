ce1fb80dc2aff0bdeda48c9f65e92c9c7ddf05eb
Taylor C. Richberger <Taywee@gmx.com>
2016-05-05 10:37:42 -0600

    update tests for latest rabbitadmin version

--- tests/test.py
+++ tests/test.py
@@ -25,43 +25,45 @@ class RabbitTest(unittest.TestCase):
         self.rabbit.delete_vhost(vhost=self.vhost)
     
     def test_user(self):
-        user = {'username': 'testuser', 'password': 'testpass'}
-        # User does not yet exist
-        with self.assertRaisesRegex(HTTPError, 'Unauthorized'):
+        with self.rabbit as rabbit:
+            user = {'username': 'testuser', 'password': 'testpass'}
+            # User does not yet exist
+            with self.assertRaisesRegex(HTTPError, 'Unauthorized'):
+                Rabbit(scheme='http', host='localhost', port=15672, **user).get_aliveness_test(vhost=self.vhost)
+
+            rabbit.put_user(user=user['username'], password=user['password'], tags='monitoring')
+
+            # User doesn't have permissions on the vhost
+            with self.assertRaisesRegex(HTTPError, 'Unauthorized'):
+                Rabbit(scheme='http', host='localhost', port=15672, **user).get_aliveness_test(vhost=self.vhost)
+
+            # User needs permissions to create and destroy the test queue
+            rabbit.put_user_vhost_permissions(vhost=self.vhost, user=user['username'], configure='^.*$', write='^.*$', read='^.*$')
+
+            # User needs the right password
+            with self.assertRaisesRegex(HTTPError, 'Unauthorized'):
+                Rabbit(scheme='http', host='localhost', port=15672, username=user['username'], password='wrongpassword').get_aliveness_test(vhost=self.vhost)
+
+            # This should work
             Rabbit(scheme='http', host='localhost', port=15672, **user).get_aliveness_test(vhost=self.vhost)
-
-        self.rabbit.put_user(user=user['username'], password=user['password'], tags='monitoring')
-
-        # User doesn't have permissions on the vhost
-        with self.assertRaisesRegex(HTTPError, 'Unauthorized'):
-            Rabbit(scheme='http', host='localhost', port=15672, **user).get_aliveness_test(vhost=self.vhost)
-
-        # User needs permissions to create and destroy the test queue
-        self.rabbit.put_user_vhost_permissions(vhost=self.vhost, user=user['username'], configure='^.*$', write='^.*$', read='^.*$')
-
-        # User needs the right password
-        with self.assertRaisesRegex(HTTPError, 'Unauthorized'):
-            Rabbit(scheme='http', host='localhost', port=15672, username=user['username'], password='wrongpassword').get_aliveness_test(vhost=self.vhost)
-
-        # This should work
-        Rabbit(scheme='http', host='localhost', port=15672, **user).get_aliveness_test(vhost=self.vhost)
-        self.rabbit.delete_user(user['username'])
+            rabbit.delete_user(user['username'])
 
     def test_queue(self):
-        queue = 'rabbitadmin_test_queue_queue'
-        exchange = 'rabbitadmin_test_queue_exchange'
-        testmessage = 'This is a test message'
-        self.rabbit.put_queue(vhost=self.vhost, queue=queue)
-        self.rabbit.put_exchange(vhost=self.vhost, exchange=exchange)
-        self.rabbit.post_bindings_by_queue(vhost=self.vhost, exchange=exchange, queue=queue)
+        with self.rabbit as rabbit:
+            queue = 'rabbitadmin_test_queue_queue'
+            exchange = 'rabbitadmin_test_queue_exchange'
+            testmessage = 'This is a test message'
+            rabbit.put_queue(vhost=self.vhost, queue=queue)
+            rabbit.put_exchange(vhost=self.vhost, exchange=exchange)
+            rabbit.post_binding_by_queue(vhost=self.vhost, exchange=exchange, queue=queue)
 
-        self.rabbit.post_exchange(vhost=self.vhost, exchange=exchange, payload=testmessage, properties={}, routing_key="", payload_encoding="string")
-        self.assertEqual(self.rabbit.post_queue_get(vhost=self.vhost, queue=queue, count=1, requeue=False, encoding="auto", truncate='50000')[0]['payload'], testmessage)
+            rabbit.post_exchange(vhost=self.vhost, exchange=exchange, payload=testmessage, properties={}, routing_key="", payload_encoding="string")
+            self.assertEqual(rabbit.post_queue_get(vhost=self.vhost, queue=queue, count=1, requeue=False, encoding="auto", truncate='50000')[0]['payload'], testmessage)
 
-        for binding in self.rabbit.get_bindings_by_queue(vhost=self.vhost, exchange=exchange, queue=queue):
-            self.rabbit.delete_binding_by_queue(vhost=binding['vhost'], exchange=binding['source'], queue=binding['destination'], props=binding['properties_key'])
-        self.rabbit.delete_exchange(vhost=self.vhost, exchange=exchange)
-        self.rabbit.delete_queue(vhost=self.vhost, queue=queue)
+            for binding in rabbit.get_bindings_by_queue(vhost=self.vhost, exchange=exchange, queue=queue):
+                rabbit.delete_binding_by_queue(vhost=binding['vhost'], exchange=binding['source'], queue=binding['destination'], props=binding['properties_key'])
+            rabbit.delete_exchange(vhost=self.vhost, exchange=exchange)
+            rabbit.delete_queue(vhost=self.vhost, queue=queue)
 
 
 
